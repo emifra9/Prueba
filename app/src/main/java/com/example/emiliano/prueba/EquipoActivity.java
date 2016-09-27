e79f08f1e77ef7388d01168a023bef1344c32fb8
Emiliano <emilianofraile9@gmail.com>
2016-09-27 15:12:16 -0300

    añade ltw de jugadores

--- app/src/main/java/com/example/emiliano/prueba/EquipoActivity.java
+++ app/src/main/java/com/example/emiliano/prueba/EquipoActivity.java
@@ -1,29 +1,31 @@
 package com.example.emiliano.prueba;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class EquipoActivity extends AppCompatActivity {
-
+public class EquipoActivity extends AppCompatActivity implements View.OnClickListener{
+private ImageButton buttonJug ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        buttonJug = (ImageButton)findViewById(R.id.btnJug);
+        buttonJug.setOnClickListener(this);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, LtwJugadoresActivity.class);
+        startActivity(i);
+
     }
 
 }
